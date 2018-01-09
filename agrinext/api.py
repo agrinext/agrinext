5734a77f433413f432401f1ef676b8dec7bfa677
Revant Nandgaonkar <revant.one@gmail.com>
2018-01-09 14:45:54 +0530

    Allow guest and user based error reporting

--- agrinext/api.py
+++ agrinext/api.py
@@ -4,9 +4,11 @@ import json
 
 @frappe.whitelist(allow_guest=True)
 def report_error():
-	if(frappe.get_request_header("X-API-KEY") == "420"):
-		note = frappe.new_doc("Note")
-		note.title = unicode(frappe.utils.datetime.datetime.today())
-		note.public = 1
-		note.content = unicode(frappe.form_dict)
+	note = frappe.new_doc("Note")
+	note.title = unicode(frappe.utils.datetime.datetime.today())
+	note.public = 1
+	note.content = unicode(frappe.form_dict)
+	if frappe.session.user == "Guest" and frappe.get_request_header("X-API-KEY") == "420":
 		note.save(ignore_permissions=True)
+	else:
+		note.save()
