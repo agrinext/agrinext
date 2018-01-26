4a562dc1195a8910e04dd7ddc47c14d0514fad85
Revant Nandgaonkar <revant.one@gmail.com>
2018-01-26 22:51:05 +0530

    file upload api

--- agrinext/api.py
+++ agrinext/api.py
@@ -2,6 +2,7 @@
 import frappe
 import json
 from frappe import _
+from frappe.utils.file_manager import save_file
 
 @frappe.whitelist(allow_guest=True)
 def report_error():
@@ -19,3 +20,37 @@ def report_error():
 @frappe.whitelist()
 def get_meta(doctype):
 	return frappe.get_meta(doctype)
+
+@frappe.whitelist()
+def upload_file():
+	'''Upload a file (POST)
+
+	:param filename: filename e.g. test-file.txt
+	:param filedata: base64 encode filedata
+	:param doctype: Reference DocType to attach file
+	:param docname: Reference DocName to attach file
+	:param folder: Folder to add File into
+	:param decode: decode filedata from base64 encode
+	:param is_private: Attach file as private file
+	:param docfield: file to attach to'''
+
+	filename = frappe.form_dict.get("filename")
+	filedata = frappe.form_dict.get("filedata")
+	doctype = frappe.form_dict.get("doctype")
+	docname = frappe.form_dict.get("docname")
+	folder = frappe.form_dict.get("folder")
+	decode = frappe.form_dict.get("decode")
+	is_private = frappe.form_dict.get("is_private")
+	docfield = frappe.form_dict.get("docfield")
+
+	if not frappe.has_permission(doctype):
+		frappe.throw(_("Not permitted"), frappe.PermissionError)
+
+	f = save_file(filename, filedata, doctype, docname, folder, decode, is_private, docfield)
+
+	if docfield and doctype:
+		doc = frappe.get_doc(doctype, docname)
+		doc.set(docfield, f.file_url)
+		doc.save()
+
+	return f.as_dict()
